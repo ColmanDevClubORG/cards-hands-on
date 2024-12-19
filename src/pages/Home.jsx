68109659b2a22c265ffbae7af983a69514e7125b
GilHeller <gil151200@gmail.com>
2024-12-19 19:25:36 +0200

    INIT

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
-import { Card } from "../components"
+import { Card } from "../components";
 
 export const Home = () => {
-    return <Card />
-}
+    return (
+        <div className="home-container">
+            {/* TODO: Implement filter by category when the user enters letters */}
+            {/* <input type="text" placeholder="Search" /> */}
+            <div className="cards-container">
+                <Card />
+            </div>
+        </div>
+    );
+};
