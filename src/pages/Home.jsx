d14b13d63656a5c2029dbcb3dd17e12a650517f5
GilHeller <gil151200@gmail.com>
2024-12-19 19:20:31 +0200

    add a usage of useState to the project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,38 @@
 import { Card } from "../components";
 import data from "../assets/data/data.json";
+import { useState } from "react";
 
 export const Home = () => {
+    const [filteredData, setFilteredData] = useState(data);
+
+    const handleChange = (e) => {
+        const text = e.target.value;
+        setFilteredData(
+            data.filter((cardData) =>
+                cardData.title
+                    .toLocaleLowerCase()
+                    .includes(text.toLocaleLowerCase())
+            )
+        );
+    };
+
     return (
-        <>
-            {data.map((place) => (
-                <Card
-                    key={place.id}
-                    {...place}
-                    // description={place.description}
-                    // image={place.image}
-                    // links={place.links}
-                    // location={place.location}
-                    // title={place.title}
-                    // key={place.id}
-                />
-            ))}
-        </>
+        <div className="home-container">
+            <input type="text" onChange={handleChange} placeholder="Search" />
+            <div className="cards-container">
+                {filteredData.map((place) => (
+                    <Card
+                        key={place.id}
+                        {...place}
+                        // description={place.description}
+                        // image={place.image}
+                        // links={place.links}
+                        // location={place.location}
+                        // title={place.title}
+                        // key={place.id}
+                    />
+                ))}
+            </div>
+        </div>
     );
 };
