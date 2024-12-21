d987db655729cbb0e0c91167a77b81f422054950
NatiF <65537815+OptimaLPro@users.noreply.github.com>
2024-12-21 15:44:16 +0200

    Init

--- src/components/commons/Card/Card.jsx
+++ src/components/commons/Card/Card.jsx
@@ -1,3 +1,13 @@
-export const Card = () => {
-    return <div className="card-container">TODO</div>;
+export const Card = ({ card }) => {
+  return (
+    <div className="card-container">
+      <img src={card.image} alt="placeholder" />
+      <h1>{card.title}</h1>
+      <h2>{card.location}</h2>
+      <p>{card.description}</p>
+      {card.links.map((link, index) => {
+        return <a href={link.url}>{link.name}</a>;
+      })}
+    </div>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,28 @@
 import { Card } from "../components";
+import CardsData from "../assets/data/data.json";
+import { useState } from "react";
 
 export const Home = () => {
-    return (
-        <div className="home-container">
-            {/* TODO: Implement filter by category when the user enters letters */}
-            {/* <input type="text" placeholder="Search" /> */}
-            <div className="cards-container">
-                <Card />
-            </div>
-        </div>
-    );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredCards = CardsData.filter((card) =>
+    card.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <div className="home-container">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+
+      <div className="cards-container">
+        {filteredCards.map((card, index) => (
+          <Card key={index} card={card} />
+        ))}
+      </div>
+    </div>
+  );
 };
