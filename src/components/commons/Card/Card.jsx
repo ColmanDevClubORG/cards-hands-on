da65f64fd94624550c3772540deae51ae38e4f29
inonELGABSI <inon11221@gmail.com>
2024-12-23 18:53:10 +0200

    sol for handzone

--- src/components/commons/Card/Card.jsx
+++ src/components/commons/Card/Card.jsx
@@ -1,3 +1,11 @@
-export const Card = () => {
-    return <div className="card-container">TODO</div>;
+export const Card = ({cardData}) => {
+    return <div className="card-container">
+        <img src={cardData.image} alt="card" />
+        <h1>{cardData.title}</h1>
+        <h2>{cardData.location}</h2>
+        <p>{cardData.description}</p>
+        {cardData.links.map((link, index) => {
+        return <a href={link.url} target="_blank" rel="noreferrer">{link.name}</a>        
+        })}
+    </div>;
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,29 @@
 import { Card } from "../components";
-
+import { useState } from "react";
+import datajson from "../assets/data/data.json";
 export const Home = () => {
+    const [filteredCards, setFilteredCards] = useState(datajson);
+
+    const handleSearch = (e) => {
+        const searchValue = e.target.value;
+        const filtered = datajson.filter((card) => {
+            return card.title.toLowerCase().includes(searchValue.toLowerCase());
+        });
+        setFilteredCards(filtered);
+    };
+
+
+
     return (
         <div className="home-container">
             {/* TODO: Implement filter by category when the user enters letters */}
-            {/* <input type="text" placeholder="Search" /> */}
+            <input type="text" placeholder="Search" onChange={handleSearch} />
             <div className="cards-container">
-                <Card />
+                {
+                filteredCards.map((cardD,index) => (
+                    <Card cardData={cardD} key={cardD.id} />
+                ))
+                }
             </div>
         </div>
     );
